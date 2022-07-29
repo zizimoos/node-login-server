f25ca17b25b3d801ed58637cf90cb0f03726229d
남형석 <azerc@MacMini.local>
2022-07-29 15:19:19 +0900

    get id value from DOM

--- src/V/public/js/login.js
+++ src/V/public/js/login.js
@@ -1,1 +1,11 @@
-alert("hello javascript");
+const id = document.getElementById("id");
+const pw = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+});
