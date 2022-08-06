de8ad9527cc36530fd30ec4aabcf5ff1685f51f5
남형석 <azerc@MacMini.local>
2022-08-06 09:06:22 +0900

    login css

--- public/js/login.js
+++ public/js/login.js
@@ -3,16 +3,15 @@ const pw = document.getElementById("password");
 const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", () => {
-  const req = {
-    id: id.value,
-    pw: pw.value,
-  };
   fetch("/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(req),
+    body: JSON.stringify({
+      id: id.value,
+      pw: pw.value,
+    }),
   })
     .then((res) => res.json())
     .then((res) => {
