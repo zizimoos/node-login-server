6ba5da5b77587dd04e011cf402dd891fa4761d73
남형석 <azerc@MacMini.local>
2022-08-16 09:20:26 +0900

    add morgan & save log file using fs system & stream

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import morgan from "morgan";
+import fs from "fs";
 
 import homeRouter from "./C/Router/home.js";
 
 const app = express();
-app.use(cors("*"));
-app.use(express.json());
-app.use(express.static(`${path.resolve()}/public`));
+const accessLogStream = fs.createWriteStream(
+  `${path.resolve()}/log/access.log`,
+  { flags: "a" }
+);
 
 app.set("view engine", "ejs");
 app.set("views", "./src/V/views");
+
+app.use(cors("*"));
+app.use(express.json());
+app.use(express.static(`${path.resolve()}/public`));
+app.use(morgan("dev"));
+app.use(
+  morgan("[:date[web]] :method :url :status", { stream: accessLogStream })
+);
 
 app.use("/", homeRouter);
 
